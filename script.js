b1381d6f507444b5ef8112d7824cc607684431be
Rafael Lima dos Santos <rafinha1990lima@gmail.com>
2022-09-02 00:13:25 -0300

    iniciando o js

---
+++ script.js
@@ -0,0 +1,20 @@
+let musica = document.querySelector('audio');
+
+document.querySelector('.botao-play').addEventListener('click', tocarMusica);
+
+document.querySelector('.botao-pause').addEventListener('click', pausarMusica);
+
+musica.addEventListener
+
+function tocarMusica(){
+    musica.play();
+    document,querySelector('botao-pause').style.display = 'block';
+    document,querySelector('botao-play').style.display = 'none';
+
+}
+
+function pausarMusica(){
+    musica.pause();
+    document.querySelector('botao-pause').style.display = 'none';
+    document.querySelector('botao-play').style.display = 'block';
+}
